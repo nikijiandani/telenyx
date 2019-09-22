b0b4757c32889a36a3ae18cf55a3dafd697e1519
Nikita Jiandani <niki.jiandani@gmail.com>
2019-09-22 11:54:54 -0400

    Working chat model

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import "./App.css";
 import Chatbar from "./components/Chatbar";
-import useFetch from "./useFetch";
 
 function App() {
-  let chat = useFetch();
-  console.log(chat);
-
   return (
     <div className="App">
       <Navbar />
-      <Chatbar data={chat} />
+      <Chatbar />
     </div>
   );
 }
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,22 +1,21 @@
 import { useState, useEffect } from "react";
 
-export default function useFetch() {
+export default function useFetch(fetchData) {
   const [data, setData] = useState([]);
   const url =
     "https://dialogflow.googleapis.com/v2/projects/telenyx-bganmh/agent/sessions/b5a0be4b-2502-a7ae-693a-2a68585f7593:detectIntent";
-  let fetchData =
-    '{"queryInput":{"text":{"text":"Hello!","languageCode":"en"}},"queryParams":{"timeZone":"America/Toronto"}}';
   useEffect(() => {
     fetch(url, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: process.env.AUTH_KEY
+        Authorization:
+          "Bearer ya29.c.ElqLB4Ul_uEZZoAQHN_Jb44ZylBYVvTet7-pRg5YniBKl6Xcs9VFmz38W_DIv1nUdx57V5X70VLnzSkC2BX6xBeOp_G4u--LYlLe59gq4c7ebZojVoxG-pVv6Xs"
       },
       body: fetchData
     })
       .then(function(response) {
-        return response.text(); // pass the data as promise to next then block
+        return response.json(); // pass the data as promise to next then block
       })
       .then(function(data) {
         setData(data);
