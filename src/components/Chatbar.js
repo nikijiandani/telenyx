d970bb2ba8c81f6f526590c8fd6d493ddb18b65f
Nikita Jiandani <niki.jiandani@gmail.com>
2019-09-22 08:17:15 -0400

    fetch bot data into app

--- src/components/Chatbar.js
+++ src/components/Chatbar.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import smallLogo from "../logo-sm.svg";
 import "../styles/chatbar.css";
 
-export default function Chatbar() {
+export default function Chatbar({ data }) {
+  const [messages, setMessages] = useState([]);
+
+  const handleMessage = e => {
+    e.preventDefault();
+    let message = {
+      firstName: "Laura",
+      content: e.target.children[0].value,
+      createdAt: new Date()
+    };
+    let time =
+      message.createdAt.getHours() + ":" + message.createdAt.getMinutes();
+    console.log(time);
+
+    let newMessageList = [...messages, message];
+    setMessages(newMessageList);
+    e.target.children[0].value = "";
+  };
+
   return (
     <main>
       <div className="chat-box">
@@ -15,8 +33,50 @@ export default function Chatbar() {
             <i className="fas fa-bars"></i>
           </span>
         </header>
-        <section>
-          <form action="POST"></form>
+        <ul className="message-list">
+          <li>{data}</li>
+          {messages.map((msg, i) => (
+            <li key={i}>{msg.content}</li>
+          ))}
+          <li>
+            <span>
+              <i className="fas fa-robot"></i>
+            </span>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
+              consequatur quidem molestias iure rem exercitationem, recusandae
+              dolorum maiores, ullam corrupti, nulla dolore ea temporibus
+              provident. Culpa nobis nesciunt architecto ex.
+            </p>
+          </li>
+          <li>
+            <span>
+              <i className="fas fa-robot"></i>
+            </span>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
+              consequatur quidem molestias iure rem exercitationem, recusandae
+              dolorum maiores, ullam corrupti, nulla dolore ea temporibus
+              provident. Culpa nobis nesciunt architecto ex.
+            </p>
+          </li>
+          <li>
+            <span>
+              <i className="fas fa-robot"></i>
+            </span>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
+              consequatur quidem molestias iure rem exercitationem, recusandae
+              dolorum maiores, ullam corrupti, nulla dolore ea temporibus
+              provident. Culpa nobis nesciunt architecto ex.
+            </p>
+          </li>
+        </ul>
+        <section className="chat-input">
+          <form onSubmit={handleMessage}>
+            <input type="text" placeholder="Enter a message" />
+            <button>Send</button>
+          </form>
         </section>
       </div>
     </main>
